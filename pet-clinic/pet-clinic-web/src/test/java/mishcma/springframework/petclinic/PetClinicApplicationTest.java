ca330bcbd1fe0b535da51504cb061f6f92837757
Konopl9 <maksimus2998@gmail.com>
2022-06-04 19:53:57 +0200

    Implemented base page and vets controller. Closes #7

---
+++ pet-clinic/pet-clinic-web/src/test/java/mishcma/springframework/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,13 @@
+package mishcma.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PetClinicApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
