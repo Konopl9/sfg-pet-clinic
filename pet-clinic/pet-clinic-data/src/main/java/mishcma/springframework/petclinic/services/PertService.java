c1191bc90616b019ce1317078e4cd8dbc22a11ad
Konopl9 <maksimus2998@gmail.com>
2022-06-04 16:22:06 +0200

    Created crud base interface for Services. Coses #11.

--- pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/PertService.java
+++ pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/PertService.java
@@ -2,13 +2,6 @@ package mishcma.springframework.petclinic.services;
 
 import mishcma.springframework.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PertService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PertService extends CrudService<Pet, Long> {
 }
--- pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/VetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package mishcma.springframework.petclinic.services;
 
 import mishcma.springframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
