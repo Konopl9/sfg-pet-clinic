0f03a7491785e102490fe647234610fb6be1d0a3
mishcma <maksym.mishchenko@storaenso.com>
2022-06-09 14:18:03 +0200

    Added pet type service

---
+++ pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package mishcma.springframework.petclinic.services;
+
+import mishcma.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic/pet-clinic-data/src/main/java/mishcma/springframework/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package mishcma.springframework.petclinic.services.map;
+
+import java.util.Set;
+import mishcma.springframework.petclinic.model.PetType;
+import mishcma.springframework.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
